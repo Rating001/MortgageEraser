4c296b70e07a9e72999aa89f749e9ebbb8cea64b
Rating001 <106230549+Rating001@users.noreply.github.com>
2022-10-05 22:47:32 -0700

    Began writing the javascript calculator

--- js/site.js
+++ js/site.js
@@ -1,4 +1,13 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+function getInput() {
+    document.getElementById('breakdown-table').classList.add("hide-me");
+
+    let loanAmount = document.getElementById('loanAmount').value;
+    let loanTerm = document.getElementById('loanTerm').value;
+    let loanInterest = document.getElementById('loanAmount').value;
+
+    calculate(loanAmount,loanTerm, loanInterest);
+}
+
+function calculate(amount,term,interest) {
+
 }

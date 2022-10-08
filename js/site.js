51b0efad7c42e25e9a02ef05e5bae20c35f40dc4
Rating001 <106230549+Rating001@users.noreply.github.com>
2022-10-07 23:14:46 -0700

    Adjusted a couple things, made the monthly payment calculation.

--- js/site.js
+++ js/site.js
@@ -1,13 +1,33 @@
 function getInput() {
     document.getElementById('breakdown-table').classList.add("hide-me");
 
-    let loanAmount = document.getElementById('loanAmount').value;
-    let loanTerm = document.getElementById('loanTerm').value;
-    let loanInterest = document.getElementById('loanAmount').value;
+    let mortgage = {};
 
-    calculate(loanAmount,loanTerm, loanInterest);
+    mortgage.loanAmount = document.getElementById('loanAmount').value;
+    mortgage.loanTerm = document.getElementById('loanTerm').value;
+    mortgage.loanInterest = document.getElementById('loanInterest').value;
+
+    //Insert the checks for specific values here.
+
+
+
+    mortgage = calculate(mortgage);
+    print(mortgage);
 }
 
-function calculate(amount,term,interest) {
+function calculate(mortgage) {
 
+
+    mortgage.monthlyPayment = (mortgage.loanAmount * (mortgage.loanInterest/1200)) / (1 - ((1 + mortgage.loanInterest/1200) ** ((-1) * mortgage.loanTerm)));
+
+    return mortgage;
+}
+
+function print(mortgage) {
+
+    let monthlyPayment = document.getElementById('monthlyPayment');
+
+    monthlyPayment.innerHTML = [];
+
+    monthlyPayment.innerHTML = "$" + mortgage.monthlyPayment.toFixed(2);
 }
